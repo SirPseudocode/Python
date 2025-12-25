aa2337e3c52a6b8cba062beab08513ee76e72e12
SirPseudocode <muhammaddaffaraditya15@gmail.com>
2025-12-25 22:57:44 +0800

    Last Four Digits

--- Getting Started/ValidateUserInput.py
+++ Getting Started/ValidateUserInput.py
@@ -1,6 +1,6 @@
 name = input("Username: ")
 
-if len(name) >= 12 or name.find(" ") != -1 or name.isdigit():
+if len(name) >= 12 or not name.isalpha():
     print(f"{name} is not accepted username")
 else:
     print(f"Welcome {name}")
