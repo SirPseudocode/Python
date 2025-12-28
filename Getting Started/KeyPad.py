2227c72cce7b5e240ffad7748132a550f7415733
SirPseudocode <muhammaddaffaraditya15@gmail.com>
2025-12-28 14:35:05 +0800

    KeyPad using 2D Collection

---
+++ Getting Started/KeyPad.py
@@ -0,0 +1,10 @@
+num_pad = ((1, 2, 3), 
+           (4, 5, 6), 
+           (7, 8, 9), 
+           ('*', 0, '#'))
+for x in num_pad:
+    for y in x:
+        print(y,  end=' ')
+    print()
+
+    
