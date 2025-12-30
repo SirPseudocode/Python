1ceedf498b7fc546a45b95b21a9fa07064dcfeee
SirPseudocode <muhammaddaffaraditya15@gmail.com>
2025-12-30 23:59:49 +0800

    Number Guessing

--- Getting Started/NumberGuessing.py
+++ Getting Started/NumberGuessing.py
@@ -8,5 +8,11 @@ while temp:
     guess = input("Quess 1 to 100 (q to quit): ").lower()
     if guess == "q":
         print(f"The answer is {ans}")
-        break
-    if()
+        temp = False
+    elif guess.isalnum() == False:
+        print(f"{guess} is Invalid Answer")
+    elif int(guess) == ans:
+        print("CORRECT!!!")
+        temp = False
+    else:
+        print(f"{guess} is incorrect")
